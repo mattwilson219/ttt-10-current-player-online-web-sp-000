7ee8acc34d793b3ac4038d205846bc3ec760233b
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-25 17:47:44 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,8 +12,8 @@ def current_player(board)
   if turn_count(board) % 2 == 0
     current_player = "X"
   else
-    current_player = "O"  
+    current_player = "O"
   end
       return current_player
-end      
+end
   
